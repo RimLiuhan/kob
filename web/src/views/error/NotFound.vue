18d201c6760a063b29c8b529f50cf7d82335c228
LH <lhisarknights@gmail.com>
2025-01-16 19:41:29 +0800

    创建导航栏和pk界面地图

---
+++ web/src/views/error/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ContentField>FOUND</ContentField> 
+    </div>
+</template>
+
+<script>
+import ContentField from '@/components/ContentField.vue';
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ContentField>用户bot</ContentField>
+    </div>
+</template>
+
+<script>
+import ContentField from '@/components/ContentField.vue';
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
